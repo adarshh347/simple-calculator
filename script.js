8207194080d806219ea2a86e00e33d02ee8d1da1
adarshh347 <adarsh347k@gmail.com>
2025-10-15 22:50:24 +0530

    added event listeners

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-let displayValue='0';
-let firstOperand=null;
-let operator=null;
-let waitingForSecondOperand = false;
+// event delegation- adding a click listener to every button
+const display = document.getElementById("display");
+const buttons = document.querySelector(".buttons");
 
-// DOM is a tree like representation in html document
-// every html tag is a node or element
-const updateDisplay = () => {
-    const display = document.getElementById('display');
-    display.textContent = displayValue;
-};
-updateDisplay();
+// diff between getElementbyId and querySelector
+// getElementbyId - display element has unique id (selects single element)
+// querySelector -  selects all elements having class buttons
+
+let currentInput=""; 
+// stores what user types
+
+buttons.addEventListener("click", (e) =>{
+    if(!e.target.classList.contains("btn")) return;  // ignore non button clicks
+
+    const value = e.target.textContent;
+    handleInput(value);
+});
